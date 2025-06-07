5a8c611c92a5712e9c2e7fc163b8e133c2a1f59b
felix corke <felix@blackspike.com>
2025-06-07 16:17:54 +0300

    better

--- app/components/Flashcard.vue
+++ app/components/Flashcard.vue
@@ -9,29 +9,25 @@ defineProps({
 
 <template>
   <div
-    class="h-full flex flex-col items-center justify-center px-8 py-12 pt-0 text-white bg-gradient-to-br from-sky-600 to-sky-800">
-    <div class="flex flex-col h-full justify-center items-center gap-4 -mt-10">
-      <div class="font-display text-[20rem] font-medium leading-none">
-        {{ letter.letter }}
-      </div>
-      <h2 class="text-2xl font-semibold -mt-10">
-        {{ letter.name }}
-      </h2>
+    class="h-full flex flex-col items-center justify-center px-8 py-12 pt-0 text-white bg-gradient-to-br from-sky-600 to-sky-800"
+  >
+    <div class="font-display text-[20rem] font-medium leading-none">
+      {{ letter.letter }}
     </div>
 
-    <div class="space-y-12">
+    <div class="space-y-12 w-full">
       <div class="grid grid-cols-2 gap-16">
         <div>
-          <h3 class="text-xl font-semibold mb-6">
-            Pronunciation
+          <h3 class="text-xl font-semibold mb-6 text-sky-300">
+            Name
           </h3>
           <p class="text-6xl">
-            {{ letter.pronunciation }}
+            {{ letter.name }}
           </p>
         </div>
 
         <div>
-          <h3 class="text-xl font-semibold mb-6">
+          <h3 class="text-xl font-semibold mb-6 text-sky-300">
             English
           </h3>
           <p class="text-6xl">
@@ -41,7 +37,7 @@ defineProps({
       </div>
 
       <div class="w-full">
-        <h3 class="text-xl font-semibold mb-6">
+        <h3 class="text-xl font-semibold mb-6 text-sky-300">
           Example - <span class="text-sky-200">{{ letter.example.english }}</span>
         </h3>
         <p class="text-6xl">
