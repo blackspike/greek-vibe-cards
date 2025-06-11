296f74270285d936ef1ca6d486a914574f86670d
Felix Corke <felix@blackspike.com>
2025-06-11 09:45:23 +0300

    better settings

--- app/pages/settings.vue
+++ app/pages/settings.vue
@@ -50,11 +50,11 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="bg-sky-600/50 py-8 px-4 rounded-2xl h-full max-h-[80dvh] flex flex-col gap-8">
-    <h1 class="text-3xl font-bold text-white text-center">Practice Stats</h1>
+  <div class="bg-sky-600/50 py-8 px-4 rounded-2xl h-full max-h-[80dvh] flex flex-col gap-3">
+    <h1 class="text-xl font-semibold text-white text-center">Practice Stats</h1>
 
     <!-- High Score Section -->
-    <div class="bg-sky-500/20 rounded-lg p-5 py-4">
+    <div class="bg-sky-500/20 rounded-lg p-4 py-3 mb-4">
       <div class="flex items-baseline justify-between text-xl font-semibold text-sky-200">
         <h2>High Score</h2>
         <div class="text-right text-white">{{ highScore }}
@@ -64,19 +64,19 @@ onMounted(() => {
 
     <div v-if="sortedFailedLetters.length > 0" class="flex-1 min-h-0">
       <div class="h-full flex flex-col">
-        <h2 class="text-xl font-semibold text-sky-200 mb-4 text-center">Frequently Missed Letters</h2>
+        <h2 class="text-lg font-semibold text-sky-200 mb-3 text-center">Frequently Missed Letters</h2>
         <div class="space-y-1 overflow-y-auto flex-1 min-h-0 pr-2">
           <div v-for="item in sortedFailedLetters" :key="item.letter"
-            class="flex items-center justify-between p-4 rounded-lg bg-sky-500/10 hover:bg-sky-500/20 transition-colors">
-            <div class="flex items-center gap-6">
-              <span class="text-4xl font-display text-white">{{ item.letter }}</span>
-              <div class="text-sky-200">
+            class="flex items-center  px-4 py-3 rounded-lg bg-sky-500/10 hover:bg-sky-500/20 transition-colors">
+            <div class="w-full items-center gap-4 flex">
+              <span class="text-4xl font-display text-center w-[2ch] text-white">{{ item.letter }}</span>
+              <div class="text-sky-200 flex-column gap-0 leading-none">
                 <p class="font-medium">{{ item.details?.name }}</p>
                 <p class="text-sm opacity-75">{{ item.details?.equivalent.toUpperCase() }}</p>
               </div>
             </div>
             <div class="flex items-center gap-6">
-              <div class="text-center">
+              <div class="text-center flex-column gap-0 leading-none">
                 <span class="font-medium text-sky-300">{{ item.count }}</span>
                 <p class="text-sm text-sky-200 opacity-75">fails</p>
               </div>
@@ -95,6 +95,17 @@ onMounted(() => {
     <div v-else class="bg-sky-500/20 rounded-lg p-6">
       <p class="text-sky-200 text-center">No practice data yet. Keep practicing to see your progress!</p>
     </div>
+
+    <footer
+      class="w-full grid grid-cols-[1fr_auto_1fr] text-sky-100/80 gap-4 [&_a]:text-sky-100/50 [&_a]:hover:text-sky-100 items-center text-sm font-semibold [&_a]:transition-colors">
+      <a href="https://www.blackspike.com">By blackspike.com</a>
+
+      <NuxtLink to="/" aria-label="home">
+        <img src="/favicon.svg" alt="Greek VibeCards" class="w-6 h-6" />
+      </NuxtLink>
+
+      <a class="text-right" href="https://github.com/blackspike/greek-vibe-cards">Code on github</a>
+    </footer>
   </div>
 </template>
 
